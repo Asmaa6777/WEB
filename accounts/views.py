f27ac35ebc7c3f2deda84173baa3508eac9025fd
rahafabdulaziz0 <rahafff005@gmail.com>
2026-05-13 02:41:14 +0300

    db

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,12 @@ def signup_view(request):
         username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
+        confirm_password = request.POST.get("confirmPassword")
+
+        if password != confirm_password:
+            return render(request, "accounts/signup.html", {
+                "error": "Passwords do not match"
+            })
 
         if User.objects.filter(username=username).exists():
             return render(request, "accounts/signup.html", {
@@ -23,26 +29,3 @@ def signup_view(request):
         return redirect("login")
 
     return render(request, "accounts/signup.html")
-
-
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-
-        user = authenticate(
-            request,
-            username=username,
-            password=password
-        )
-
-        if user is not None:
-            login(request, user)
-            return redirect("login")
-
-    return render(request, "accounts/login.html")
-
-
-def logout_view(request):
-    logout(request)
-    return redirect("login")
