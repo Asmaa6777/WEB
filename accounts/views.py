cddf9dee7030fb0081e0ea9e1566ff14fdcaa852
rahafabdulaziz0 <rahafff005@gmail.com>
2026-05-13 01:26:45 +0300

    m

--- accounts/views.py
+++ accounts/views.py
@@ -23,7 +23,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect("/pages/homepage.html")
+          return redirect("login")
 
     return render(request, "accounts/login.html")
 
